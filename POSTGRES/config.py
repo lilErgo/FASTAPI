e7783d87061e6178494dda70cac8d9b098e716f6
LE <egarussiago@gmail.com>
2026-02-04 15:59:34 +1000

    alembic заработал

--- POSTGRES/config.py
+++ POSTGRES/config.py
@@ -33,7 +33,7 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL_psycopg(self):
         
-        return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        return f'postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
     @property
     def REDIS_URL(self):
--- POSTGRES/models/crypto_table.py
+++ POSTGRES/models/crypto_table.py
@@ -1,15 +1,25 @@
-from sqlalchemy.orm import DeclarativeMeta, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from typing import Optional
 
-class crypto_info(DeclarativeMeta):
-    __tablename__ = "crypto_info"
+class Base(DeclarativeBase):
+    # УБРАТЬ это: id: Any
+    # УБРАТЬ это: __name__: str (это не нужно)
+    
+    __allow_unmapped__ = False  # лучше False если не уверены
 
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+class crypto_info(Base):
+    # __tablename__ будет "cryptoinfo" из Base.__tablename__
+    # но у вас есть явное объявление ниже, которое переопределит
+    
     id: Mapped[int] = mapped_column(primary_key=True)
     price: Mapped[int]
     time: Mapped[str]
 
-class crypto_tag(DeclarativeMeta):
-    __tablename__ = "crypto_tags"
-
+class crypto_tag(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
 
@@ -26,27 +36,43 @@ class crypto_tag(DeclarativeMeta):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 # from sqlalchemy import Column, Integer, String, Float
 # from sqlalchemy.sql import func
-# from ...config import settings
+# from ..config import settings
 
-# class Cryptocoin(settings): # type: ignore
-#     __tablename__ = "Cryptocoin"
+# class CryptoInfo(settings): # type: ignore
+#     __tablename__ = "crypto_info"
 
 #     id = Column(Integer, primary_key=True, index=True,autoincrement=True)
 #     name = Column(String, index=True)
 #     price = Column(Float)
 #     time = Column(Float)
+
+# class CryptoTag(settings): # type: ignore
+#     __tablename__ = "crypto_tags"
+
+#     id = Column(Integer, primary_key=True, index=True,autoincrement=True)
+#     name = Column(String, index=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    
